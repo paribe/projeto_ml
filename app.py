e8fa4178c179ea0bf40e8b7b317701bdc5285a69
paribe <paribe@gmail.com>
2024-07-31 16:39:08 -0300

    Terceira versão

--- app.py
+++ app.py
@@ -17,10 +17,16 @@ modelo.fit(x,y)
 st.title("Prevendo valor de uma pizza")
 
 st.divider()
+# Usar st.text_input para permitir entrada nula
+diametro_input = st.text_input("Digite o tamanho do diâmetro da pizza em cm:")
 
-diametro = st.number_input("Digite o tamanho do diametro da pizza em cm: ")
-
-if diametro:
-    preco_previsto = modelo.predict([[diametro]])[0][0]
-    st.write(f"O valor da pizza do diâmetro de {diametro:.2f} cm é de R$ {preco_previsto:.2f}")
-    st.balloons()
+if diametro_input:
+    try:
+        diametro = float(diametro_input)
+        preco_previsto = modelo.predict([[diametro]])[0][0]
+        st.write(f"O valor da pizza do diâmetro de {diametro:.2f} cm é de R$ {preco_previsto:.2f}")
+        st.balloons()
+    except ValueError:
+        st.error("Por favor, insira um valor numérico válido para o diâmetro.")
+else:
+    st.write("Por favor, insira o diâmetro da pizza para calcular o valor.")
